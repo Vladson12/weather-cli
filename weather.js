dbcc82c664a998d28e56f5c7685a135d1cc0a39f
Vladislav Magomedov <vladdmagg94@yandex.ru>
2022-12-19 22:14:24 +0300

    feat: add storage

--- weather.js
+++ weather.js
@@ -2,11 +2,26 @@
 import process from 'process';
 import { getArgs } from './helpers/args.js';
 import { printError, printHelp, printSuccess } from './services/log-service.js';
+import { saveKeyValue } from './services/storage-service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Token saved');
+  } catch (err) {
+    printError(err.message);
+  }
+};
 
 const initCli = () => {
   const args = getArgs(process.argv);
   if (args.h) {
     printHelp();
+  }
+  if (args.s) {
+  }
+  if (args.t) {
+    return saveToken('token', args.t);
   }
 };
 
